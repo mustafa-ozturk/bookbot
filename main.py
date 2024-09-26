14edc7033793cecab351bfb7b3aa5d93d4f0f031
mustafa-ozturk <mozturkpro@gmail.com>
2024-09-25 22:11:09 -0400

    add print repor t

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ def main():
     book_path = "books/frankenstein.txt";
     text = get_book_text(book_path)
     wc = get_word_count(text)
-    char_count_dict = get_character_dict(text)
-    print(char_count_dict)
+    char_count_dict = get_char_count_dict(text)
+    char_count_dict_list = get_char_count_dict_list(char_count_dict)
+    char_count_dict_list.sort(reverse=True, key=sort_on)
+    print_report(book_path, wc, char_count_dict_list)
 
 def get_book_text(path):
     with open("books/frankenstein.txt") as f:
@@ -12,7 +14,7 @@ def get_book_text(path):
 def get_word_count(text):
     return len(text.split());
 
-def get_character_dict(text):
+def get_char_count_dict(text):
     chars = {}
     for c in text:
         lc = c.lower()
@@ -20,5 +22,24 @@ def get_character_dict(text):
             chars[lc] = 0
         chars[lc] += 1
     return chars
+
+def get_char_count_dict_list(dict):
+    char_count_dict_list = [];
+    for key in dict:
+        char_count_dict_list.append({"char": key, "count": dict[key]})
+    return char_count_dict_list;
+
+def sort_on(dict):
+    return dict["count"]
+
+def print_report(book_path, wc, list):
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{wc} words found in the document\n")
+
+    for cc in list:
+        if cc["char"].isalpha():
+            print(f"The '{cc["char"]} character was found '{cc["count"]}' times")
+    print(f"--- End Report ---")
+
 
 main()
