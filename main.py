013993c81e0a2197730110c018100eb2e80e505d
mustafa-ozturk <mozturkpro@gmail.com>
2024-09-25 21:42:11 -0400

    add get word count

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 def main():
     book_path = "books/frankenstein.txt";
     text = get_book_text(book_path)
-    print(text)
+    wc = get_word_count(text)
+    print(wc)
 
 def get_book_text(path):
     with open("books/frankenstein.txt") as f:
         return f.read()
+
+def get_word_count(text):
+    return len(text.split());
 
 main()
