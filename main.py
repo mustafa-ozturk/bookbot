0f880b5d0c90e6739379fccfb8b2bf725865b5ce
mustafa-ozturk <mozturkpro@gmail.com>
2024-09-25 21:35:47 -0400

    print book content

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world")
+def main():
+    book_path = "books/frankenstein.txt";
+    text = get_book_text(book_path)
+    print(text)
+
+def get_book_text(path):
+    with open("books/frankenstein.txt") as f:
+        return f.read()
+
+main()
